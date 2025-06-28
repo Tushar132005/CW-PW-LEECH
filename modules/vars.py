98472f90fc5fb7ab8a805a9af7b178fb9c0dfcc1
HKsuccessspark <successspark09@gmail.com>
2025-06-28 13:01:09 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,4 @@ import os
 
 api_id = "24798261"
 api_hash = "fef280037f5759eccc540c6d7a279a14"
-bot_token = "7691523346:AAFqKCQJZSMbXioa9Q8fExiTDarsBcRynYA"
+bot_token = "7691523346:AAHegf5Sl0LdyaaIYNKDlez-ZO8f1Ls8o5I"
